df49fb328eb686c965ae79343695c9f931c95967
MarynaPr <pryadkamaryna@gmail.com>
2022-04-10 15:21:07 -0500

    animation for name and intro

---
+++ src/components/Home/index.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+import './index.scss'
+import LogoTitle from '../../assets/images/logo.png'
+import { Link } from 'react-router-dom'
+import AnimatedLetters from '../AnimatedLetters'
+
+function Home() {
+
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['m', 'a', 'r', 'y', 'n', 'a']
+    const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.']
+
+    useEffect(() => {
+        return setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+    }, [])
+    return (
+        <>
+            <div className='container home-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <span className={letterClass}>H</span>
+                        <span className={`${letterClass}_12`}>i,</span>
+                        <br />
+                        <span className={`${letterClass}_13`}>I</span>
+                        <span className={`${letterClass}_14`}>'m</span>
+
+                        <img src={LogoTitle} alt="developer" />
+                        <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} /> <br />
+                        <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={22} /> <br />
+                    </h1>
+                    <br />
+                    <h2>Frontend Developer / Code Expert /110 Tabs Opener</h2>
+                    <Link to='/contact' className='flat-button'>CONTACT ME</Link>
+                </div>
+            </div>
+
+        </>
+
+    )
+}
+
+export default Home
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 import Sidebar from '../Sidebar'
 import './index.scss'
 
@@ -6,9 +7,21 @@ import './index.scss'
 function Layout() {
     return (
 
-        <>
+        <div className='App'>
             <Sidebar />
-        </>
+            <div className='"page'>
+                <span className='tags top-tags'>
+                    &lt;body&gt;
+                </span>
+                <Outlet />
+                <span className='tags bottom-tags'>
+                    &lt;/body&gt;
+                    <br />
+
+                    <span className='bottom-tag-html'>&lt;/html&gt;</span>
+                </span>
+            </div>
+        </div>
 
 
     )
